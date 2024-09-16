cf1c662e9cf6482313e6eb3a866400fdc5596854
Augusto Rufino <joseaorufino@gmail.com>
2024-09-15 23:01:07 -0300

    chore: add set for maxmemory policy

--- redis/src/resize.py
+++ redis/src/resize.py
@@ -1,7 +1,9 @@
 import redis
 
 r = redis.Redis(host="localhost", port=6380)
-new_maxmemory = int(r.config_get('maxmemory')['maxmemory']) * 0.5
+new_maxmemory = int(r.config_get('maxmemory')['maxmemory']) * 0.1
+
+r.config_set('maxmemory-policy', 'allkeys-lfu') # default is noeviction
 r.config_set('maxmemory', int(new_maxmemory))
 
 print("Redis maxmemory is resized.")
