453312b7e1048a3ea569a09e3c75ec33fc7a0cba
Augusto Rufino <joseaorufino@gmail.com>
2024-09-15 23:02:03 -0300

    chore: remove verification of used memory > maxmemory

--- redis/src/fill_redis.py
+++ redis/src/fill_redis.py
@@ -13,16 +13,12 @@ try:
     while True:
         key = f"key{count}"
         list_value = ["v" * 1024 for _ in range(100)]
-        r.rpush(key, *list_value) # TODO: Erro: command not allowed when used memory > 'maxmemory'.
+        r.rpush(key, *list_value)
 
-        used_memory = r.info('memory')['used_memory']
-        if used_memory >= max_memory: # Verificação desnecessária
-            print(f"Limite de memória atingido: {used_memory} bytes")
-            break
-        
         count += 1
       
 except redis.exceptions.ResponseError as e:
-    used_memory = r.info('memory')['used_memory']
-    print(f"Limite de memória atingido: {used_memory} bytes")
-    print(f"Erro: {e}")
+    if "command not allowed when used memory > 'maxmemory'" in str(e):
+        print(f"Limite de memória atingido: {r.info('memory')['used_memory']} bytes")
+    else:
+        print(f"Erro: {e}")
